c70856d3d1756cb24a6f5cbbedea2d8e175aac44
Ashim Roy <118077929+ashim-roy@users.noreply.github.com>
2026-07-14 22:59:45 +0530

    addd AuditingEntityListener

--- src/main/java/com/Ashim/CommerceEngine/ProductServiceApplication.java
+++ src/main/java/com/Ashim/CommerceEngine/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package com.Ashim.CommerceEngine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing     // to audit last modified, updated, these are not updating automatically.
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/Ashim/CommerceEngine/userService/models/BaseModel.java
+++ src/main/java/com/Ashim/CommerceEngine/userService/models/BaseModel.java
@@ -1,11 +1,11 @@
 package com.Ashim.CommerceEngine.userService.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 import java.util.UUID;
@@ -14,11 +14,16 @@ import java.util.UUID;
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreatedDate
     private Date createdAt;
+
+    @LastModifiedDate
     private Date lastModifiedAt;
    //private Boolean deleted;   // Deleted for if token is active or not, token is active or not.
    private Boolean deleted = false;
