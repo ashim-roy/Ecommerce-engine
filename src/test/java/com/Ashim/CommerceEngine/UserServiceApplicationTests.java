8befb9cd3430ca8a9fee69728f7b8f6a03bbfde6
Ashim Roy <118077929+ashim-roy@users.noreply.github.com>
2026-07-23 20:52:49 +0530

    added testcase and saved into db

--- src/test/java/com/Ashim/CommerceEngine/UserServiceApplicationTests.java
+++ src/test/java/com/Ashim/CommerceEngine/UserServiceApplicationTests.java
@@ -1,11 +1,26 @@
 package com.Ashim.CommerceEngine;
 
 
+import com.Ashim.CommerceEngine.userService.security.services.JpaRegisteredClientRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class UserServiceApplicationTests {
+
+    @Autowired
+    private JpaRegisteredClientRepository  registeredClientRepository;
 
 
     @Test
@@ -14,6 +29,26 @@ public class UserServiceApplicationTests {
 
     @Test
     void addClientToDB(){
+                RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("oidc-client")
+                .clientSecret("{noop}secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
+                .postLogoutRedirectUri("http://127.0.0.1:8080/")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+
+               registeredClientRepository.save(oidcClient);
+
+               // Since this is a JUnit test, it's a good practice to verify the insert instead of just saving.
+        RegisteredClient savedClient =
+                registeredClientRepository.findByClientId("oidc-client");
+
+        assertNotNull(savedClient);
 
     }
 
