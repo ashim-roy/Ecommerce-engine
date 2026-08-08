229eefff278cf38295213b31e3a6c5028bde7794
Ashim Roy <118077929+ashim-roy@users.noreply.github.com>
2026-08-08 23:48:35 +0530

    added payment sevrice

--- PaymentService/src/main/java/com/ashim/PaymentService/services/PaymentService.java
+++ PaymentService/src/main/java/com/ashim/PaymentService/services/PaymentService.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class PaymentService {
 
+    public String initiatePayment(Long orderId, String phoneNumber) {
+        return null;
+    }
+
 }
