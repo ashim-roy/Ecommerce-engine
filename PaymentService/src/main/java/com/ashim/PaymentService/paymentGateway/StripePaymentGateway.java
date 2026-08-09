f6a9518795018f270e01662f1500d75c53d80b55
Ashim Roy <118077929+ashim-roy@users.noreply.github.com>
2026-08-09 23:50:29 +0530

    added Stripe payment gateway

--- PaymentService/src/main/java/com/ashim/PaymentService/paymentGateway/StripePaymentGateway.java
+++ PaymentService/src/main/java/com/ashim/PaymentService/paymentGateway/StripePaymentGateway.java
@@ -1,10 +1,15 @@
 package com.ashim.PaymentService.paymentGateway;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StripePaymentGateway implements PaymentGateway {
+
+    @Value("${stripe.api.key}")
+    private String apiKey;
+
     @Override
     public String initiatePayment(Long orderId, String phoneNumber) {
         return null ;
