1bff850676f79f5208bac56594f1022bd1373d6f
Ashim Roy <118077929+ashim-roy@users.noreply.github.com>
2026-08-09 23:55:57 +0530

    added Stripe payment gateway code

--- PaymentService/src/main/java/com/ashim/PaymentService/paymentGateway/StripePaymentGateway.java
+++ PaymentService/src/main/java/com/ashim/PaymentService/paymentGateway/StripePaymentGateway.java
@@ -1,6 +1,8 @@
 package com.ashim.PaymentService.paymentGateway;
 
 
+import com.stripe.model.PaymentLink;
+import com.stripe.param.PaymentLinkCreateParams;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +14,22 @@ public class StripePaymentGateway implements PaymentGateway {
 
     @Override
     public String initiatePayment(Long orderId, String phoneNumber) {
-        return null ;
+
         // make a call to Stripe payment gateway in order to generate payment link
+        PaymentLinkCreateParams params =
+                PaymentLinkCreateParams.builder()
+                        .addLineItem(
+                                PaymentLinkCreateParams.LineItem.builder()
+                                        .setPrice("{{PRICE_ID}}")
+                                        .setQuantity(1L)
+                                        .build()
+                        )
+                        .build();
+
+// For SDK versions 29.4.0 or lower, remove '.v1()' from the following line.
+        PaymentLink paymentLink = client.v1().paymentLinks().create(params);
+
+        return null ;
+
     }
 }
